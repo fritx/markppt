3f00eaa81e742d04612fcd45d2a3ff01df5b47b5
Fritz Lin <uxfritz@163.com>
2015-04-07 22:17:39 +0800

    init

---
+++ mytalk/_ppt/ppt.js
@@ -0,0 +1,111 @@
+// 为了方便 重写fn.show/hide
+// 使用visibility属性 实现占位
+$.fn.show = function(){
+  return $(this).css('visibility', 'visible')
+}
+$.fn.hide = function(){
+  return $(this).css('visibility', 'hidden')
+}
+
+var current = 0 // 当前页索引
+var $secs = $('section')
+init()
+
+function init() {
+  // 侦听键盘事件 前后页切换  
+  $(document).on('keydown', function(e){
+    var code = e.keyCode
+    if (code === 40 || code === 39) { // 右/下 前进
+      go(1)
+    }
+    else if (code === 37 || code === 38) { // 左/上 后退
+      go(-1)
+    }
+  })
+
+  clear() // 清除全部
+  show(0) // 显示首页
+  window.onload = function(){
+    $(window).on('resize', layout)
+    style() // 装饰
+    layout() // 布局
+    layout() // hack 再次调用
+    $('body>div').show()
+  }
+}
+
+function style() {
+  var max = 256
+  var range = 50
+  $secs.each(function(i, sec){ // 每页随机着浅色
+    var colors = [
+      max-range*(1-Math.random()) | 0,
+      max-range*(1-Math.random()) | 0,
+      max-range*(1-Math.random()) | 0
+    ]
+    $(sec).css({
+      'background-color': 'rgb('+ colors.join(',') +')'
+    })
+  })
+}
+
+function layout() {
+  var W = window.innerWidth
+  var H = window.innerHeight
+  if (W <= H) { // 横屏
+    $secs.each(function(i, sec){
+      var $div = $(sec).children('div')
+      var h = $div.height()
+      $div.css({
+        'position': 'absolute',
+        'padding': '0 10%',
+        'top': 100*.45 + '%',
+        'margin-top': (-h/2) + 'px'
+      })
+      if (h > H/(1.5-.45)) { // 内容高度超出范围 需缩放
+        $div.css({
+          'top': 100*.5 + '%',
+          '-webkit-transform': 'scale('+ H/h +')'
+        })
+      }
+    })
+  }
+  else { // 竖屏
+    $secs.each(function(i, sec){
+      var $div = $(sec).children('div')
+      var h = $div.height()
+      $div.css({
+        'position': 'absolute',
+        'padding': '0 20%',
+        'top': 100*.47 + '%',
+        'margin-top': (-h/2) + 'px'
+      })
+      if (h > H/(1.5-.47)) { // 内容高度超出范围 需缩放
+        $div.css({
+          'top': 100*.5 + '%',
+          '-webkit-transform': 'scale('+ H/h +')'
+        })
+      }
+    })
+  }
+}
+
+function go(step) {
+  var next = current + step
+  if (next < 0 || next >= $secs.length) {
+    return
+  }
+  clear() // 清除全部
+  show(next) // 显示下一页
+  hide(current) // 隐藏当前页
+  current = next
+}
+function clear() {
+  $secs.hide().css('z-index', 0).removeClass('animated zoomInUp zoomOutDown')
+}
+function show(index) {
+  $secs.eq(index).css('z-index', 2).addClass('animated zoomInUp').show()
+}
+function hide(index) {
+  $secs.eq(index).css('z-index', 3).addClass('animated zoomOutDown').show()
+}
---
+++ mytalk/talk.md
@@ -0,0 +1,23 @@
+# Talk
+
+*Date*
+
+## Heading 1
+
+Paragraph 1
+
+Paragraph 1-1
+
+## Heading 2
+
+![Desert](Desert.jpg)
+
+Paragraph 2
+
+## Heading 3
+
+Paragraph 3
+
+- Item 3-1
+- Item 3-2
+- Item 3-3
