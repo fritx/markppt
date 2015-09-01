ee0be71efe9e1c9ca7eec59021a2ae660ea70f87
Jayin Ton <tonjayin@gmail.com>
2015-09-01 18:05:59 +0800

    .handlebars => .hbs

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 var fs = require('fs-extra')
 var path = require('path')
-var Handlebars = require('handlebars');
+var Handlebars = require('handlebars')
 
 exports.build = build
 
-function build(file, opt) {
+function build (file, opt) {
   if (!opt.theme) opt.theme = 'dark'
 
-  var basename = path.basename(file) //=> index.md
-  var baseseg = basename.split('.')[0] //=> index
+  var basename = path.basename(file) // => index.md
+  var baseseg = basename.split('.')[0] // => index
   opt.url = basename
 
-  var source = fs.readFileSync(path.join(__dirname, './web/ppt.handlebars'), {encoding: 'utf8'})
+  var source = fs.readFileSync(path.join(__dirname, './web/ppt.hbs'), {encoding: 'utf8'})
   var template = Handlebars.compile(source, {noEscape: true})
-  var out = template({"opt": JSON.stringify(opt)})
+  var out = template({'opt': JSON.stringify(opt)})
 
   var dir = path.dirname(file)
   fs.copySync(path.resolve(__dirname, 'web', 'ppt_'), path.resolve(dir, 'ppt_'))
-  fs.writeFileSync(path.resolve(dir, baseseg + '.html'), out) //=> index.html
+  fs.writeFileSync(path.resolve(dir, baseseg + '.html'), out) // => index.html
 }
